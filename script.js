28dbf9af07a24cd2b29dccc3151855a318552a4e
panagiotis_papantonis <panagiotispapantonis1@gmail.com>
2023-06-03 12:23:24 +0300

    back to normal ( didnt work)

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
-
-
-
-  
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
@@ -36,35 +32,8 @@ let weather = {
       "Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
 
-    //renia
-    const pexelsApiKey = 't55JsDB4A5n1Bm1dtI74rhLK65M9abqIvvNp2gbztN4BSXjVnS2OQhrc';
-    const pexelsApiUrl = "https://api.pexels.com/v1/search?query=${?city}&per_page=1";
-
-    fetch(pexelsApiUrl, {
-      headers: {
-        Authorization: pexelsApiKey
-      }
-    })
-      .then(response => response.json())
-      .then(photoData => {
-        if (photoData && photoData.photos && photoData.photos.length > 0) {
-          const photoUrl = photoData.photos[0].src.large2x;
-          document.body.style.backgroundImage = `url(${photoUrl})`;
-        } else {
-           document.body.style.backgroundImage =
-              "url('https://source.unsplash.com/1600x900/?" + name + "')";
-        }
-      })
-      // .catch(error => {
-      //   console.error('Error:', error);
-      //   document.querySelector('.img').style.backgroundImage = '';
-      // });
-      //telos renia
-
-
-
-                // document.body.style.backgroundImage =
-                //   "url('https://source.unsplash.com/1600x900/?" + name + "')";
+                document.body.style.backgroundImage =
+                  "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
@@ -82,5 +51,4 @@ document
       weather.search();
     }
   });
-
 
